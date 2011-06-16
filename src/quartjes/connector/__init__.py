209554d4eedfbe3cccad900b057cf33f9aa5087d
seelfester <seelfester@37a75b76-5144-4623-9033-b160cbee6dce>
2011-06-16 16:05:52 +0000

    Added little description of the connector.

--- src/quartjes/connector/__init__.py
+++ src/quartjes/connector/__init__.py
@@ -1,2 +1,23 @@
-__author__="rob"
+"""
+Quartjes Connector
+
+Allows connections between the Quartjes server and its clients.
+
+
+Server components
+
+A server must start the ServerConnector in quartjes.connector.server
+For each component you want to be accessible through the connector implement
+a subclass of quartjes.connector.services.Service and register it with the
+ServerConnector
+
+Client components
+
+To connect to the server create a ClientConnector from quartjes.connector.client
+and start it. On the ClientConnector call get_service_interface to get an interface
+to call methods on a specific service on the server.
+
+"""
+
+__author__="Rob van der Most"
 __date__ ="$May 27, 2011 8:52:57 PM$"
