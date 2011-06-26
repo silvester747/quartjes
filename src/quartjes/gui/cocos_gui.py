adc1fab87c6997a002f0a027ca72a6228b77145c
seelfester <seelfester@37a75b76-5144-4623-9033-b160cbee6dce>
2011-06-26 16:44:47 +0000

    Made a more funky ticker

--- src/quartjes/gui/cocos_gui.py
+++ src/quartjes/gui/cocos_gui.py
@@ -1,57 +1,67 @@
-import cocos.scenes.transitions
-import cocos.director
-import cocos.text
-
 __author__="rob"
 __date__ ="$Jun 23, 2011 10:13:35 PM$"
 
 import cocos
 from cocos.actions import *
 from cocos.particle_systems import *
+import cocos.scenes.transitions
+import cocos.director
+import cocos.text
+from quartjes.controllers.database import Database
 
-class DemoLayer(cocos.layer.Layer):
+class TitleLayer(cocos.layer.Layer):
 
     def __init__(self):
-        super(DemoLayer, self).__init__()
+        super(TitleLayer, self).__init__()
         
-        demo_label = cocos.text.Label("Proof of Concept",
+        demo_label = cocos.text.Label("QuartjesAvond",
                                       font_name='Times New Roman',
                                       font_size=80,
-                                      anchor_x='center', anchor_y='center')
-        demo_label.position = (512, 400)
+                                      anchor_x='center', anchor_y='top')
+        demo_label.position = (512, 768)
         self.add(demo_label)
         demo_label.do(Repeat(Waves3D(duration=200, waves=200)))
-
-        fire = Fire()
-        fire.position = (512, 500)
-        self.add(fire)
-
-        sun = Sun()
-        sun.position = (712, 500)
-        self.add(sun)
-
-        meteor = Meteor()
-        meteor.position = (312, 500)
-        self.add(meteor)
-
 
 
 class BottomTicker(cocos.layer.Layer):
 
-    def __init__(self):
+    def __init__(self, display_time=1.0, display_y=0, screen_width=1024, drinks=None):
         super(BottomTicker, self).__init__()
 
-        self.drinks = ["Cola - 4", "Bier - 10", "Sinas - 8", "Vodka - 1"]
+        self.display_time = display_time
+        self.display_y = display_y
+        self.screen_width = screen_width
+        self.visible_segments = 8
+        self.margin = 1
+        self.interval = 2
+        self.focus_segment = 5
+        self.focus_length = 2
+        self.focus_height = 40
+
+        self._calculate_points()
+
+        self.drinks = drinks
         self.current_drink = 0
 
-        self.next_drink(None, None)
+        self.next_drink()
+
+    def _calculate_points(self):
+
+        distance = self.screen_width / self.visible_segments
+        self.points = [(x*distance, self.display_y) for x in range(self.visible_segments + self.margin, -1 - self.margin, -1)]
+        self.points[self.focus_segment] = (self.points[self.focus_segment][0], self.focus_height)
+
+        self.segment_count = len(self.points) - 1
+
+        self.focus_start = self.focus_segment - (self.focus_length / 2)
+        self.focus_end = self.focus_segment + (self.focus_length / 2)
+
+
 
     def update_drinks(self, drinks):
         self.drinks, self.current_drink = drinks, 0
 
-    def next_drink(self, current_label, next_label):
-        if next_label != None:
-            next_label.kill()
+    def next_drink(self):
 
         text = ""
 
@@ -59,18 +69,29 @@ class BottomTicker(cocos.layer.Layer):
             self.current_drink += 1
             if self.current_drink >= len(self.drinks):
                 self.current_drink = 0
-            text = self.drinks[self.current_drink]
+            drink = self.drinks[self.current_drink]
+            text = "%s - %d" % (drink.name, drink.sellprice())
 
         next_label = cocos.text.Label(text,
                                  font_name='Times New Roman',
                                  font_size=64,
-                                 anchor_x='center', anchor_y='center')
-        next_label.position = 1424,50
+                                 anchor_x='center', anchor_y='bottom')
+        next_label.position = self.points[0]
         next_label.scale = 0.5
         self.add(next_label)
 
-        next_label.do((MoveTo((512, 50), 1) | ScaleTo(1, 1)) + Delay(2) + CallFunc(self.next_drink, next_label, current_label) + (ScaleTo(0.5, 1) | MoveTo((-400, 50), 1)))
+        spawn_actions = Delay(self.display_time * self.interval) + CallFunc(self.next_drink)
 
+        move_actions = MoveTo(self.points[self.focus_start], self.display_time * self.focus_start)
+        move_actions += (MoveTo(self.points[self.focus_segment], self.display_time * (self.focus_length / 2)) |
+                         (Delay((self.display_time * (self.focus_length / 2)) / 2) +
+                         ScaleTo(1, (self.display_time * (self.focus_length / 2)) / 2)))
+        move_actions += (MoveTo(self.points[self.focus_end], self.display_time * (self.focus_length / 2)) |
+                         (ScaleTo(0.5, (self.display_time * (self.focus_length / 2)) / 2)) +
+                         Delay((self.display_time * (self.focus_length / 2)) / 2))
+        move_actions += MoveTo(self.points[-1], self.display_time * (self.segment_count - self.focus_end))
+
+        next_label.do((spawn_actions | move_actions) + CallFunc(next_label.kill))
 
 
 if __name__ == "__main__":
@@ -78,16 +99,11 @@ if __name__ == "__main__":
     def transition_to(dest):
         cocos.director.director.replace(cocos.scenes.transitions.ShrinkGrowTransition(dest))
 
+    db = Database()
+
     cocos.director.director.init(width=1024, height=768, fullscreen=True)
     ticker_layer = BottomTicker()
-    demo_layer = DemoLayer()
-    color_layer = cocos.layer.util_layers.ColorLayer(255, 170, 127, 255)
-    main_scene = cocos.scene.Scene(ticker_layer, demo_layer)
-    other_scene = cocos.scene.Scene(color_layer, ticker_layer)
-    main_scene.do(Delay(10) + CallFunc(transition_to, other_scene))
-    other_scene.do(Delay(10) + CallFunc(ticker_layer.update_drinks, None) +
-                   Delay(10) +
-                   CallFunc(ticker_layer.update_drinks,
-                            ["Cola - 4", "Bier - 10", "Sinas - 8", "Vodka - 1"]) +
-                   Delay(10) + CallFunc(transition_to, main_scene))
+    ticker_layer.update_drinks(db.drinks)
+    title_layer = TitleLayer()
+    main_scene = cocos.scene.Scene(ticker_layer, title_layer)
     cocos.director.director.run(main_scene)
