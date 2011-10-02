c18c59cd946517fc6bbb8699c1d3d87dc44894d1
pietbraat <pietbraat@37a75b76-5144-4623-9033-b160cbee6dce>
2011-10-02 00:16:14 +0000

    Joost - update sell_dialog. Price update still not working

--- src/quartjes/gui/sell_dialog.py
+++ src/quartjes/gui/sell_dialog.py
@@ -30,33 +30,41 @@ class dialogSell(Frame):
         else:
             return
         self.sex = self.conn.get_service_interface("stock_exchange")
-        self.sex.sell(drink[selected], amount)
+        self.sex.sell(drink[self.selected], amount)
 
     def fill_drinks_listbox(self,lb_drinks):
         self.drinks = self.db.get_drinks()
-        names = []
         for d in self.drinks:
             lb_drinks.insert(END,d.name)
 
     def calc_price(self):
-        self.entryvalue = self.sv_amount.get()
+        self.entryvalue = self.e_amount.get()
         if len(self.entryvalue) >0:
-            amount = entryvalue
+            amount = int(self.entryvalue)
         else:
             return
         self.selection = self.lb_drinks.curselection()
+
         if len(self.selection) > 0:
-            selected = int(selection[0])
+            selected = int(self.selection[0])
         else:
             return
-        self.newprice = amount * self.drink[selected].sellprice_quartjes
-        self.pricevar = "price: "+self.newprice
+        self.drinks = self.db.get_drinks()
+        self.newprice = amount * int(self.drinks[selected].sellprice_quartjes())
+        print(self.newprice)
+        self.sv_price.set(self.newprice)
+
+    def printklik(self, eventdata):
+        print("klik")
+
 
     def createWidgets(self):
         font16 = ("Arial", 26, "bold")
         font12 = ("Arial", 18, "bold")
         self.lb_drinks = Listbox(self, height=40, width = 100)
         self.lb_drinks.grid(row = 0,column=0,rowspan=10,sticky=EW, padx = 20, pady = 20)
+        self.lb_drinks.bind("<Button-1>", self.printklik)
+
         self.fill_drinks_listbox(self.lb_drinks)
 
         self.frame1 = Frame(self)
@@ -70,16 +78,15 @@ class dialogSell(Frame):
         self.e_amount.grid(row = 1,column=1,sticky=EW, padx = 0, pady = 0)
         self.e_amount.insert(0,1)
 
+        self.sv_price = StringVar()
+        self.l_price = Label(self.frame1, textvariable = self.sv_price, width = 20, height = 0, font = font16)
+        self.l_price.grid(row = 2,column=1,sticky=EW, padx = 20, pady = 0)
+
         self.b_update_price = Button(self.frame1, width = 10, text = "Update Price", font = font12, command = self.calc_price)
         self.b_update_price.grid(row = 1, column = 2)
 
         self.b_sell = Button(self, text = "Sell", bg="#999999", command = self.sell_drink, activebackground="#ff5555", width = 20, height = 2, font = font16)
         self.b_sell.grid(row = 1,column=1,sticky=EW, padx = 20, pady = 20)
-
-        self.pricevar = StringVar()
-        self.pricevar = "price: "
-        self.l_price = Label(self, textvariable=self.pricevar, width = 20, height = 0, font = font16)
-        self.l_price.grid(row = 2,column=1,sticky=EW, padx = 0, pady = 0)
 
 
 if __name__ == "__main__":
