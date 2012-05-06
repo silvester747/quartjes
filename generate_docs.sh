b495c13ef7e5aa889e2ee411134c149abc0bf77d
seelfester <seelfester@37a75b76-5144-4623-9033-b160cbee6dce>
2012-05-06 11:03:54 +0000

    Added documentation.

---
+++ generate_docs.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+epydoc --html -o doc --name "Quartjes" src/quartjes
--- install_deps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-apt-get install python
-apt-get install python-tk
-apt-get install python-twisted-bin python-twisted-core 
-apt-get install python-numpy
-apt-get remove python-pyglet
