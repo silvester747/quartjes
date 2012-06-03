5fa8d8611ea6aa3eb5fbf0b1c92f108b9452ab1f
pietbraat <pietbraat@37a75b76-5144-4623-9033-b160cbee6dce>
2012-06-03 12:48:52 +0000

    sales client update

--- src/quartjes/gui/Sales_client.py
+++ src/quartjes/gui/Sales_client.py
@@ -1,4 +1,3 @@
-import time
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
 
@@ -12,20 +11,25 @@ from sell_dialog import dialogSell
 import tkMessageBox
 
 class Sales_client(Frame):
-    def __init__(self, root,hostname,port):
+    def __init__(self, root):
         root.title('Sales client')
         Frame.__init__(self, root)
         self.root = root
         self.pack()
-        self.createWidgets()     
+        self.createWidgets()
+        self.conn = ClientConnector() 
 
-        self.conn = ClientConnector(hostname,port)
-
-    def connect_to_server(self):
+    def connect_to_server(self):        
         if self.conn.is_connected():
             self.conn.stop()
-            self.b_connect_to_server.config(text = "Not connected", bg="#ff0000",activebackground="#ff5555")
-        else:            
+            self.b_connect_to_server.config(text = "Not connected to server", bg="#ff0000",activebackground="#ff5555")
+        else:                        
+            try:
+                self.conn.host = str(self.e_server_hostname.get())
+                self.conn.port = int(self.e_server_port.get())
+            except:
+                pass                
+            
             self.conn.start()
             self.b_connect_to_server.config(text = "Connected", bg="#00ff00",activebackground="#55ff55")
 
@@ -44,24 +48,35 @@ class Sales_client(Frame):
         else:
             tkMessageBox.showwarning("Not connected to server","Please connected to a server first.")
 
-
     def createWidgets(self):
         font16 = ("Arial", 26, "bold")
         
-        self.b_edit_dialog = Button(self, text = "Edit database",command =  self.edit_db, width = 20, height = 2, font = font16)
-        self.b_edit_dialog.grid(row = 1,column=0,sticky=EW, padx = 20, pady = 20)
-
-        self.b_sell_dialog = Button(self, text = "Sell dialog", command = self.sell, width = 20, height = 2, font = font16)
-        self.b_sell_dialog.grid(row = 2,column = 0,sticky=EW, padx = 20, pady = 20)
-
-        self.b_connect_to_server = Button(self, text = "Not connected", bg="#ff0000", activebackground="#ff5555", command =  self.connect_to_server, width = 20, height = 2, font = font16)
-        self.b_connect_to_server.grid(row = 0,column=0,sticky=EW, padx = 20, pady = 20)
+        # frame 1
+        self.frame1=Frame(self, width = 20, height = 2)
         
+        Label(self.frame1, text="Server hostname/IP:",font = font16).grid(row=0)
+        Label(self.frame1, text="Server port:",font = font16).grid(row=1)
+
+        self.e_server_hostname = Entry(self.frame1, font = font16)
+        self.e_server_port = Entry(self.frame1, font = font16)       
+        self.b_connect_to_server = Button(self.frame1, text = "Connect to server", bg="#ff0000", activebackground="#ff5555", command =  self.connect_to_server, font = font16)
+        # populate frame
+        self.e_server_hostname.grid(row=0, column=1)
+        self.e_server_port.grid(row=1, column=1)
+        self.b_connect_to_server.grid(row=2,columnspan=2)        
+        # end frame one
+
+        self.b_edit_dialog = Button(self, text = "Edit database",command =  self.edit_db, width = 20, height = 2, font = font16)
+        self.b_sell_dialog = Button(self, text = "Sell dialog", command = self.sell, width = 20, height = 2, font = font16)
+
+        self.frame1.grid(row = 0,column=0,sticky=EW, padx = 20, pady = 20)        
+        self.b_edit_dialog.grid(row = 1,column=0,sticky=EW, padx = 20, pady = 20)
+        self.b_sell_dialog.grid(row = 2,column = 0,sticky=EW, padx = 20, pady = 20)
+        
+        #return self.e_server_hostname # initial focus
+       
 if __name__ == "__main__":    
-    hostname = None
-    port = 1234
-    
     root = Tk()
-    app = Sales_client(root,hostname,port)
+    app = Sales_client(root)
     app.mainloop()
     
