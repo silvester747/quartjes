933e145e10385cd4cdcf0b1ac80ad9b32f202669
seelfester <seelfester@37a75b76-5144-4623-9033-b160cbee6dce>
2012-07-07 19:51:08 +0000

    Better algorithm for selling mostly less expensive drinks.

--- src/quartjes/controllers/test/sales_tester.py
+++ src/quartjes/controllers/test/sales_tester.py
@@ -1,6 +1,7 @@
 import random
 import time
 from quartjes.connector.client import ClientConnector
+from quartjes.models.drink import Mix
 
 
 __author__="rob"
@@ -15,22 +16,37 @@ if __name__ == "__main__":
     database = con.database
     exchange = con.stock_exchange
 
-    current_drinks = database.get_drinks()
+    current_drinks = {}
+    max_position = 0
 
     def update_drinks(drinks):
         global current_drinks
-        current_drinks = drinks
+        global max_position
+        
+        new_drinks = {}
+        position = 0
+        for drink in drinks:
+            #print(drink)
+            new_drinks[position] = drink
+            position += int(1000.0 / drink.price_factor)
+        
+        current_drinks, max_position = new_drinks, position
+        
+        #print(current_drinks)
+            
+
+    update_drinks(database.get_drinks())
 
     database.on_drinks_updated += update_drinks
 
     while True:
-        i = random.randint(0, len(current_drinks) - 1)
+        rand_pos = random.randint(0, max_position)
+        pos = 0
+        for p in current_drinks.keys():
+            if p > pos and p <= rand_pos:
+                pos = p
 
-        #if current_drinks[i].price_factor > 1.4:
-        #    continue
-
-        #a = random.randint(1, 6 - int((current_drinks[i].price_factor - 1) * 10))
         a = random.randint(1, 6)
 
-        exchange.sell(drink=current_drinks[i], amount=a)
-        time.sleep(5.0)
+        exchange.sell(drink=current_drinks[pos], amount=a)
+        time.sleep(1.0)
