23cfee258d1f727e58ab7f3a68cf18b7cd21a516
seelfester <seelfester@37a75b76-5144-4623-9033-b160cbee6dce>
2013-07-07 12:26:20 +0000

    Start of a new feature that was still lingering on my machine: trend watcher. To be continued for 2014 ;-)

---
+++ src/quartjes/controllers/trendwatcher.py
@@ -0,0 +1,35 @@
+"""
+Calculations to determine trends in sales and prices.
+
+Functionality
+-------------
+- Get list ordered by percentage increase/decrease price
+
+
+Limitations
+-----------
+- Assuming equal intervals between history items, ignore timestamps
+
+@author Rob
+"""
+
+from quartjes.controllers.database import database
+
+def calculate_absolute_factor_difference_data():
+    """
+    Get all drink history from the database. Calculate for each history item
+    the difference with the current price.
+    """
+    drinks = database.get_drinks()
+    
+    data = {}
+    
+    # Fill history data for each drink
+    for drink in drinks:
+        current_price_factor = drink.price_factor
+        factor_diffs = []
+        for history_item in reversed(drink.price_history):
+            factor_diffs.append(current_price_factor / history_item.price_factor)
+        data[drink] = factor_diffs
+    
+    return data      
