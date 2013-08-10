004d7ce7e4d1cb3d021a86493816d926e591a167
seelfester <seelfester@37a75b76-5144-4623-9033-b160cbee6dce>
2013-08-10 18:32:01 +0000

    Make in place update of nodes prettier.

--- src/quartjes/gui/cocos/basenodes.py
+++ src/quartjes/gui/cocos/basenodes.py
@@ -3,7 +3,7 @@ Basic nodes used in different parts of the Cocos GUI.
 """
 
 import cocos
-from cocos.actions import FadeOut
+from cocos.actions import FadeIn, FadeOut
 import pyglet
 from pyglet.gl import glPushMatrix, glPopMatrix
 
@@ -20,13 +20,29 @@ class LabelBatch(cocos.cocosnode.CocosNode):
         """
         super(LabelBatch, self).__init__()
         self.position = position
-        self.group = None
-        self.elements = []
+        self._group = None
+        self._elements = []
+        
+        self._opacity = None
 
         if self.use_batch_rendering:
-            self.batch = pyglet.graphics.Batch()
+            self._batch = pyglet.graphics.Batch()
         else:
-            self.batch = None
+            self._batch = None
+
+    @property
+    def opacity(self):
+        '''
+        Opacity of all elements contained. Returns None if not set explicitly.
+        Note: if you directly set opacity on any of the contained objects, this will ignore that.
+        '''
+        return self._opacity
+    
+    @opacity.setter
+    def opacity(self, value):
+        self._opacity = value
+        for e in self._elements:
+            e.color = tuple(e.color[:3]) + (int(value),)
 
     def add_text(self, text='', position=(0,0), **kwargs):
         """
@@ -34,9 +50,9 @@ class LabelBatch(cocos.cocosnode.CocosNode):
         Accepts additional keyword arguments to pass on to a pyglet label.
         """
         kwargs['text']=text
-        element = pyglet.text.Label(group=self.group, batch=self.batch,
+        element = pyglet.text.Label(group=self._group, batch=self._batch,
                                     x=position[0], y=position[1], **kwargs)
-        self.elements.append(element)
+        self._elements.append(element)
 
     def draw(self):
         """
@@ -45,10 +61,10 @@ class LabelBatch(cocos.cocosnode.CocosNode):
         """
         glPushMatrix()
         self.transform()
-        if self.batch:
-            self.batch.draw()
+        if self._batch:
+            self._batch.draw()
         else:
-            for e in self.elements:
+            for e in self._elements:
                 e.draw()
         glPopMatrix()
 
@@ -70,12 +86,13 @@ class UpdatableNode(cocos.cocosnode.CocosNode):
         """
         Update the drink shown.
         """
-        #self._inner_node.do(FadeOut(1))
-        self._inner_node.kill()
+        self._inner_node.do(FadeOut(0.5))
+        #self._inner_node.kill()
         self._inner_node = new_node
+        new_node.opacity = 0
         self.add(new_node, z=self._next_z_level)
         self._next_z_level += 1
-        #self._inner_node.do(FadeIn(1))
+        self._inner_node.do(FadeIn(0.5))
     
     def hide(self):
         self._inner_node.do(FadeOut(1))
